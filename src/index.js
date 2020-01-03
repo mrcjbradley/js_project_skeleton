de6a59f21b5943db82e7ea77b695d9cb75346f1a
mrcjbradley <cbradley@appacademy.io>
2020-01-03 10:58:14 -0500

    Add optional-chaining

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import './styles/index.scss';
-
-window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('app').innerText = "Hello World!";
+import "./styles/index.scss";
+const testObj = {
+  key1: "hi",
+  key2: {
+    key3: "Hello"
+  }
+};
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("app").innerText = `${testObj?.key2?.key3 ||
+    testObj.key1} World!`;
 });
