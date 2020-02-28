283f39b71cfe557a31463d5a297aa769b01c8e74
mrcjbradley <cjbradley@me.com>
2020-02-28 14:44:51 -0500

    Render res to page

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@ import axios from "axios";
 
 window.addEventListener("DOMContentLoaded", () => {
   document.getElementById("app").innerText = "Hello World!";
-  axios.get("./search?").then(res => console.log(res));
+  axios
+    .get("./search?")
+    // .then(res => res.json())
+    .then(({ data: { docs } }) => {
+      docs.forEach((doc, idx) => {
+        const newH3 = document.createElement("h3");
+        newH3.innerText = doc.title_suggest;
+        const newP = document.createElement("p");
+        newP.innerText = doc.first_sentence;
+        doc.first_sentence && document.body.append(newH3, newP);
+      });
+    });
 });
